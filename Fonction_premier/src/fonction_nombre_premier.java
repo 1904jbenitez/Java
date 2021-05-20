f62376312a5a775e9627498a2c464e0828a2299e
1904jbenitez <75559577+1904jbenitez@users.noreply.github.com>
2021-05-20 16:01:15 +0200

    mise à jour principal des dossiers

---
+++ Fonction_premier/src/fonction_nombre_premier.java
@@ -0,0 +1,91 @@
+import java.util.*;
+
+public class fonction_nombre_premier {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	Scanner sc =new Scanner(System.in);
+
+	System.out.println("Veuillez saisir un nombre entier"); 
+
+	long nbtest=sc.nextLong();
+
+	long [] diviseurs= new long[50];	
+
+	diviseurs=rechercheDiviseur ( nbtest); 
+	
+	for (int i = 0; i < diviseurs.length; i++)
+	{
+	if (diviseurs[i]!=0)
+	{ 
+		System.out.println("Le numero indiqué n'est pas un numéro premier\n Diviseurs : "+diviseurs[i]);
+		
+	}else 
+		{
+		System.out.println("Le numero indiqué est un numéro premier");
+		}
+		sc.close();
+	}
+
+
+/*
+ * String recommencer=""; long nbtest;
+ * 
+ * boolean premier; do { premier= true; long diviseur=2;
+ * 
+ * System.out.println("Veuillez saisir un nombre entier"); nbtest=sc.nextLong();
+ * while(diviseur<=Math.sqrt(nbtest) && premier==true) { if (nbtest%diviseur==0)
+ * {
+ * 
+ * premier=false;
+ * 
+ * 
+ * }
+ * 
+ * 
+ * 
+ * diviseur++;
+ * 
+ * }
+ * 
+ * if (!premier) { System.out.println("Le nombre "
+ * +nbtest+" n'est pas premier car il a au moins un diviseur :"+ (diviseur-1));
+ * 
+ * } else { System.out.println("Le nombre "+nbtest+" est premier" ); }
+ * sc.nextLine();
+ * 
+ * System.out.println(" Voulez-vous recommencer? O/N");
+ * 
+ * recommencer=sc.nextLine();
+ * 
+ * 
+ * } while (recommencer.charAt(0)=='O' && recommencer.length()<2 );
+ */
+
+
+	}
+
+
+	public static long [] rechercheDiviseur ( long _unNombre ) 
+	{
+		long [] mesDiviseurs= new long[50];
+		int compteurCase=0;
+		for (int i = 2; i<_unNombre ; i++) 
+		{
+
+	if (_unNombre%i==0) {
+
+		mesDiviseurs[compteurCase]=i;
+		compteurCase++;
+		
+
+	}
+
+	
+		}
+
+		return mesDiviseurs;
+	}	
+	
+}
